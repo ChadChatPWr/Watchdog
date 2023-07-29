1c0c3513fba99aa4f26ed52a5c852d240f213e1a
vixu321 <vixu19@gmail.com>
2023-07-29 16:18:32 +0200

    Added functionality to deathManager

--- src/deathManager.cpp
+++ src/deathManager.cpp
@@ -1,14 +1,59 @@
 #include "deathManager.h"
+#include <signal.h> //Lib for kill function
 
 namespace watchdog{
 
 deathManager::deathManager(){
 	std::cout << "start test deathManager" << std::endl;
 }
-void deathManager::handleUnresponsive(std::string idComponent){}
-void deathManager::kill(std::string idComponent){}
-void deathManager::checkIsKIlled(std::string idComponent){}
-void deathManager::reset(std::string idComponent){}
+
+void deathManager::handleUnresponsive(pid_t idComponent){
+	//Check if the PID is correct
+	if(idComponent){
+		//Check if PID is running
+		if(checkIsKIlled){
+			//PID not running, restart the process
+			std::cout<<"PID not running, restarting the process"<<std::endl;
+			reset(idComponent);
+		}else{
+			//PID running, but unresponsive, kill the PID
+			std::cout<<"PID runnning, but unresponsive, killing the thread"<<std::endl;
+			killThread(idComponent);
+		}
+	}
+	
+}
+
+//idComponent is passed on as pid_t, so that the kill function from signal.h library can work correctly.
+void deathManager::killThread(pid_t idComponent){
+	std::cout<<"Attempting to kill thread with PID number "<<idComponent<<std::endl;
+		if(idComponent){
+			if(kill(idComponent, SIGTERM)==0){
+				//Successful kill
+				kill(idComponent, SIGTERM);
+				std::cout<<"Successfully killed thread with PID number "<<idComponent<<std::endl;
+			}else{
+				//An error occured during kill function
+				std::cout<<"An error occured during kill function"<<std::endl;
+			}
+
+		}
+}
+
+//Check if PID is killed, return false if its still running, return true if it isnt running
+bool deathManager::checkIsKIlled(pid_t idComponent){
+	if(0==kill(idComponent, 0)){
+		//Still running
+		std::cout<<"Thread with PID number "<<idComponent<<" is still running."<<std::endl;
+		return false;
+	}else{
+		//Not running
+		std::cout<<"Thread with PID number "<<idComponent<<" is not running."<<std::endl;
+		return true;
+	}
+}
+//No real way to do reset function through deathManager code, each component needs to have its own restart function that the deathManager can access.
+void deathManager::reset(pid_t idComponent){}
 deathManager::~deathManager(){
 	std::cout << "stop test deathManager" << std::endl;
 }
