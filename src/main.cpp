2f9c8d020269fa126e73699c10f4ef140d1aae3a
bxris95 <266138@student.pwr.edu.pl>
2023-08-06 16:57:24 +0200

    06.08.2023

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+
 #include "watchdog.h"
 #include "component.h"
 #include "deathManager.h"
 #include "lifeMonitor.h"
 
+
+bool shouldCreateNewTask = true;
+
+
 int main()
 {
-	watchdog::watchdog watchdog;
+    watchdog::watchdog watchdog;
+    watchdog.monitorComponent();
 	
+    std::atomic<bool> shouldClose;
+    shouldClose = false;
+
+
+
+    std::cout << "id of main thread is:" << std::this_thread::get_id() << std::endl;
+
+    if(shouldCreateNewTask){ //later will be if(recive())
+
+    std::string s = "WebApp";
+    pid_t l = 1;
+
+    watchdog.setidComponent(l);
+    watchdog.setcomponentName(s);
+
+    watchdog.createComponent(watchdog.getidComponent(),watchdog.getcomponentName() ,shouldClose);
+}
+
+
+
 	return 0;
 }
--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -2,17 +2,57 @@
 
 namespace watchdog{
 
+
+
 watchdog::watchdog()
 {
 	std::cout << "start watchdog" << std::endl;
 }
+void watchdog::setcomponentName(std::string componentName){
+
+    watchdog::componentName = componentName;
+
+}
+
+void watchdog::setidComponent(pid_t idComponent){
+    watchdog::idComponent = idComponent;
+}
+
+std::string watchdog::getcomponentName(){
+    return watchdog::componentName;
+}
+
+pid_t watchdog::getidComponent(){
+    return watchdog::idComponent;
+}
+
+void watchdog::createComponent(const pid_t& idComponent,const std::string& componentName, std::atomic<bool> &shouldClose){
+
+    std::mutex mtx;
+
+    std::lock_guard<std::mutex>lock(mtx);
+
+    std::thread([&]() {
+        component component(idComponent,componentName);
+        shouldClose = true;
+
+    }).detach();
+
+
+    while(!shouldClose){
+        std::this_thread::yield();
+    }
+}
+
 void watchdog::monitorComponent()
 {
+
 }
-void watchdog::createComponent(std::string idComponent){}
-void watchdog::createLifeMonitor(std::string idComponent){}
+
+
 watchdog::~watchdog()
 {
 	std::cout << "stop watchdog" << std::endl;
+
 }
 }
